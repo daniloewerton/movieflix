84c9ea24b6341d18464af05a4e127b47275c6009
Danilo Ewerton <danilo.ewe@gmail.com>
2024-02-27 09:27:23 -0300

    configurações iniciais

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Hello from 'components/Hello';
+import './assets/styles/custom.scss';
+import './App.css';
+
+function App() {
+
+  return (
+    <Hello />
+  );
+}
+
+export default App;
---
+++ src/components/Hello/index.tsx
@@ -0,0 +1,8 @@
+function Hello() {
+
+    return (
+        <h1 className="primary">Ola mundo</h1>
+    );
+}
+
+export default Hello;
